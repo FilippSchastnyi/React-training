c51f1e3508a65ed56675ca33a2d688d2e48496de
Pilip.Shchasny <c-pshchasny@neogov.net>
2021-10-01 22:05:05 +0300

    added bootstrap and the table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useHttp from './hooks/http-hook';
+import Table from './components/Table/Table';
 
-const App: React.FC = () => (
-  <h1>Please read README.md</h1>
-)
+const App: React.FC = () => {
+  const { request } = useHttp();
+  const [cardOperationsData, setCardOperationsData] = useState();
 
+  const getCardOperationsData = async () => {
+    try {
+      const response = await request(`https://api-dev.fidel.uk/v1d/programs/`, 'GET', null, {
+        'fidel-key': 'sk_test_8b665908-284c-4dd1-a364-7ebc575c18f6',
+      });
+      if (response) {
+        setCardOperationsData(response.items);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getCardOperationsData();
+  }, []);
+
+  if (cardOperationsData === false) return null;
+
+  console.log(cardOperationsData);
+
+  return (
+    <div className="container">
+      <Table />
+    </div>
+  );
+};
 export default App;
--- src/task-1/task-1.ts
+++
@@ -1,5 +0,0 @@
-interface Tree extends Record<string, Tree | string> {}
-
-export default function flattenTree(tree: Tree): Record<string, string> {
-  return {};
-}
